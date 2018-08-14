93c683daeeeac47aea4dde3bb500e1202e80902d
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2018-08-14 09:09:58 -0300

    FGB | Inicializando

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>Controle de usuários</title>
+
+    <meta charset="UTF-8" />
+  </head>
+
+  <body>
+    <nav>
+    </nav>
+
+    <section>
+      <article>
+      </article>
+    </section>
+
+    <aside>
+    </aside>
+
+    <footer>
+    </footer>
+  </body>
+</html>
